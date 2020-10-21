45f4fd948dc9b40a14b87259f3c8eecc7cfe8320
kadeksuryam <kadeksuryam@gmail.com>
2020-10-21 22:44:58 +0700

    added about, skills, projects, contact sections

--- main.js
+++ main.js
@@ -16,3 +16,45 @@ selectElement(".burger-menu-icon").addEventListener("click", ()=>{
     })
 });
 
+navLinks.forEach(link => {
+    link.addEventListener("click", ()=>{
+        selectElement(".nav-list").classList.toggle("active");
+        selectElement(".burger-menu-icon").classList.toggle("toggle");
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = "";
+            }else{
+                link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
+            }
+        })
+    })
+})
+
+
+function openEmail(){
+	window.open("mailto:kadeksuryam@gmail.com?subject=<edit the subject>");
+}
+function openFacebook(){
+	window.open("https://www.facebook.com/surya.mahardika.184", "_blank");
+}
+function openInstagram(){
+	window.open("https://www.instagram.com/suryam1729/", "_blank");
+}
+function openLinkedIn(){
+	window.open("https://www.linkedin.com/in/kadek-surya-m-695256122/", "_blank");
+}
+function openGithub(){
+	window.open("https://github.com/kadeksuryam", "_blank");
+}
+function openCV(){
+	window.open("doc/resume.pdf", "_blank");
+}
+function openProject1(){
+	window.open("https://github.com/kadeksuryam/Algeo01-13519044", "_blank");
+}
+function openProject2(){
+	window.open("https://github.com/kadeksuryam/Tubes_Daspro_IF1210", "_blank");
+}
+function openProject3(){
+	window.open("https://github.com/kadeksuryam/Tubes_Daspro_IF1210", "_blank");
+}
